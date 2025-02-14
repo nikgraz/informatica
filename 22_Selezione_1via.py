bb5c2bd00071986821787ce266c949d680145a92
nikgraz <graziano.nicholas@itisgrassi.edu.it>
2025-02-14 12:24:03 +0100

    modifica

---
+++ 22_Selezione_1via.py
@@ -0,0 +1,6 @@
+print("Benvenuto nel mio programma di conversazione")
+print("Ti piace andare in bicicletta?")
+risposta=input("Rispondi sì o no: ")
+if risposta=="sì":
+ print("Molto bene! Ti terrai in forma.")
+print("Ciao ciao.")
