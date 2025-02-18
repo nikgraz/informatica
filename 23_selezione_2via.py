7d83d5093a623295686d176582184032e9ea63af
nikgraz <graziano.nicholas@itisgrassi.edu.it>
2025-02-18 12:02:48 +0100

    Add files via upload

---
+++ 23_selezione_2via.py
@@ -0,0 +1,8 @@
+print("Benvenuto nel mio programma di conversazione")
+print("Ti piace andare in bicicletta?")
+risposta=input("Rispondi sì o no: ")
+if risposta=="sì":
+ print("Molto bene! Ti terrai in forma.")
+else:
+ print("Forse ti piace qualche altro sport.")
+print("Ciao ciao.")
---
+++ 24_Selezione_3vie.py
@@ -0,0 +1,10 @@
+print("Benvenuto nel mio programma di conversazione")
+print("Ti piace andare in bicicletta?")
+risposta=input("Rispondi sì o no: ")
+if risposta=="sì":
+ print("Molto bene! Ti terrai in forma.")
+elif risposta=="no":
+ print("Forse ti piace qualche altro sport.")
+else:
+ print("Risposta errata, dovevi rispondere sì o no!")
+print("Ciao ciao.")
