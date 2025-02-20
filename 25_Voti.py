2ff4d13d6106706fd3782416a23f3d0f855f7ee6
nikgraz <graziano.nicholas@itisgrassi.edu.it>
2025-02-20 10:15:07 +0100

    Add files via upload

---
+++ 25_Voti.py
@@ -0,0 +1,8 @@
+print("Come è andata la verifica?")
+voto = float(input("Inserisci un voto da 0 a 10: "))
+if voto > 6:
+ print("Bene. Hai preso più della sufficienza!")
+elif voto == 6:
+ print("voto sufficente")
+else:
+ print("Purtroppo il voto è insufficente")
