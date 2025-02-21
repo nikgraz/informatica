67d381b33f805ec44ddcfa9017535722e4d5bff1
nikgraz <graziano.nicholas@itisgrassi.edu.it>
2025-02-21 12:16:29 +0100

    Add files via upload

---
+++ 01_InputOutput.py
@@ -0,0 +1,5 @@
+nome=input('Ciao, come ti chiami? ') #Funzione che chiede nel terminale il nome dell'utente e salva la risposta nella variabile 'nome'
+print("Ciao ", nome) #Funzione che stampa nel terminale la scritta 'Ciao (nome inserito precedentemente)'
+print('Vogliamo sapere se ti piace programmare!') #Funzione che stampa nel terminale una domanda all'utente
+risposta=input('Ti piace programmare? ') #Funzione che propone una domanda nel terminale e prende la risposta come variabile
+print('Bene, hai scritto: ', risposta) #Funzione che scrive nel terminale 'Bene, hai scritto: (risposta aggiunta precedentemente)'
